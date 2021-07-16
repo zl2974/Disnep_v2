b158f5b10064b5787870031a4ecdec42abf76820
Your Name <you@example.com>
2021-07-16 12:03:15 -0400

    still fail in 2000 settings

---
+++ R/RWR.R
@@ -0,0 +1,48 @@
+### Random Walk with restart
+
+RWR = function(signals,
+               trans,
+               beta,
+               normalize_type = 1,
+               .max_iter = 100,
+               .tol = 1e-6
+){
+  ### normalize transition matrix
+  
+  diag(trans) = 0
+  
+  source("R/normalize.R")
+  trans = normalize(trans,type = normalize_type)
+  
+  ### check signals tibble
+  
+  signals = arrange(signals,gene)
+  
+  p0 = signals$score
+  
+  current = signals$score
+  
+  previous = current+Inf
+  
+  ### random walk with restart
+  
+  .iter = 1
+  
+  while(abs(norm(current-previous))>.tol & .iter < .max_iter){
+    
+    previous = current
+    
+    current = beta * trans%*%previous + (1-beta)*p0
+    
+  }
+  
+  ### collect result
+  
+  result = tibble(gene = signals$gene,
+                  score = current) %>% 
+    arrange(score)
+  
+  return(result)
+  
+  
+}
