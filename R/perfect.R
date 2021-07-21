edac74cf2a4d4edaf945bc3168a3741e05fd1c11
Your Name <you@example.com>
2021-07-21 10:14:02 -0400

    perfect transition matrix

---
+++ R/perfect.R
@@ -0,0 +1,18 @@
+#### perfect transition matrix
+
+perfect = function(signals,n_signal,n_noise,corr){
+  
+  source("R/diffus_vec.R")
+  
+  a = matrix(corr,nrow = n_signal,ncol = n_signal)
+  
+  b = matrix(1e-3,nrow=n_noise,ncol = n_noise)
+  
+  adjacency = as.matrix(Matrix::bdiag(a,b))
+  
+  diag(adjacency) = 0
+  
+  result = as_tibble(diffus_vec(signals,adjacency,type = "statistics",beta = 0.75,iter = 100))
+  
+  return(result)
+}
